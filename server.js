bc6713317d07546f964f2ddfedb111ebe6a9055e
aayushtiwari <arsenicaayush@gmail.com>
2019-04-13 14:49:10 +0530

    make it better

--- server.js
+++ server.js
@@ -49,9 +49,6 @@ app.get("/result", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(`${__dirname}/client/build`));
-
   // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
     res.sendFile(`${__dirname}/client/build`, "index.html");
